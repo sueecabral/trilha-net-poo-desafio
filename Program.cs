d578c0052497d4c4afbe18cf8a8da7993678504d
Suelen Cabral <sueecabral@gmail.com>
2023-12-29 11:33:58 -0300

    Alteração de propriedades e metodos das classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//numero, modelo, imei, memoria
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(81)9 9999-9999","1100","222222222222222", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone ("(81)9 9999-9999","13","333333333333333", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
